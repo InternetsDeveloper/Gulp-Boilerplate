3f63fa1a59a2c242d589935f375a1ff3d2f6f12d
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-15 14:43:30 -0500

    navbar

--- source/app.js
+++ source/app.js
@@ -1,2 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var NavBar = React.createClass({
+    render: function () {
+        return <nav > {
+                this.props.appName
+            } < ul > < li > < a href = {
+                this.props.home
+            } > {
+                this.props.home
+            } < /a></li >
+            < li > < a href = {
+                this.props.contact
+            } > {
+                this.props.contact
+            } < /a></li >
+            < /ul> < /nav > ;
+    }
+
+});
