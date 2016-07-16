af3843de70d54b10d3c66a08c2325e1de52a4466
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-16 18:05:05 -0500

    task add

--- gulpfile.uglify.old.js
+++ gulpfile.uglify.old.js
@@ -5,3 +5,23 @@ var source = require('vinyl-source-stream');
 var watch = require('gulp-watch');
 
 //NOTES: gulp.task(nameOfTask, callback);
+
+watch(['./source/*.js'], function () {
+    console.log("App has been changed... One moment");
+    gulp.start('default');
+});
+
+gulp.task('default', function () {
+            return browserify('./source/app.js')
+                .transform("babelify", {
+                    presets: ["es2015", "react"]
+                })
+                .bundle()
+                .pipe(source('build.js'))
+                .pipe(gulp.dest('./build/'))
+
+            var uglify = require('gulp-uglify');
+            var concat = require('gulp-concat');
+            var watch = require('gulp-watch');
+
+            console.log('Gulpfile loaded.  Ready to run tasks.');
