f64f4f22aa263cffb52e914fb7b30b0c9f535db3
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-16 18:07:59 -0500

    global settings add

--- gulpfile.uglify.old.js
+++ gulpfile.uglify.old.js
@@ -25,3 +25,23 @@ gulp.task('default', function () {
             var watch = require('gulp-watch');
 
             console.log('Gulpfile loaded.  Ready to run tasks.');
+
+            // global settings for gulp
+            var settings = {
+                scriptsFolder: 'scripts/',
+                distFolder: 'dist/'
+            };
+
+            // watch for changes!
+            watch([settings.scriptsFolder + 'app/*.js'], function () {
+                console.log('oh snap! scripts have been changed... let us rebundle');
+                gulp.start('default'); // run default task when changes occur
+            });
+
+            // default gulp task
+            gulp.task('default', function () {
+                return gulp.src([settings.scriptsFolder + 'lib/jquery.js', settings.scriptsFolder + 'app/*.js'])
+                    .pipe(concat('distribution-app.js'))
+                    .pipe(uglify()) // comment out uglify for debugging OR development
+                    .pipe(gulp.dest(settings.distFolder));
+            });
