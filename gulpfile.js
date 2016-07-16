2eae7409fff72f14b2ff2acca9623b0b5037c89b
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-16 17:24:59 -0500

    task

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,14 @@ watch(['./source/app.js'], function () {
     console.log("App has been changed; re-compiling for best results.");
     gulp.start('default');
 });
+
+gulp.task('default', function () {
+        return browserify('./source/app.js')
+            .transform("babelify", {
+                presets: ["es2015", "react"]
+            })
+            .bundle()
+            .pipe(source('build.js'))
+            .pipe(gulp.dest('./build/'))
+    )
+};
