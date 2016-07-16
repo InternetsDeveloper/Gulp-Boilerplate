fbb1f57f64edac895c71a2eea959886db3ee793d
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-16 18:00:03 -0500

    require

---
+++ gulpfile.uglify.old.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var watch = require('gulp-watch');
+
+//NOTES: gulp.task(nameOfTask, callback);
